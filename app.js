63318ee5f7230cdb2c90f3557021977411a64201
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-04-23 14:02:24 +0530

    Added headers to prevent CORS errors

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-express = require('express');
-productsRoute = require('./routes/api/products');
-ordersRoute = require("./routes/api/order");
+const bodyParser = require('body-parser');
+const express = require('express');
+const productsRoute = require('./routes/api/products');
+const ordersRoute = require("./routes/api/order");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origins, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if( req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Method', 'PUT,GET,POST,DELETE,PATCH');
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
@@ -14,7 +31,7 @@ app.use((req, res, next) => {
     next(error);
 })
 
-app.use((next, req, res, next) => {
+app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {
