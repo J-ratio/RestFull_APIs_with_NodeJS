f8aaf3a4076d8171af70e254b1c032189f075872
r_@tio <f20201505@goa.bits-pilani.ac.in>
2023-04-23 12:57:50 +0530

    added default error handling

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ const app = express();
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
 
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404;
+    next(error);
+})
+
+app.use((next, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
